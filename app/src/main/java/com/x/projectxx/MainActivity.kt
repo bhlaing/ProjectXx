543bd5303127a4174f658bfd44f316269caab347
Billy <billhlaing@gmail.com>
2020-07-30 17:19:06 +1000

    facebook login set up with button

--- app/src/main/java/com/x/projectxx/MainActivity.kt
+++ app/src/main/java/com/x/projectxx/MainActivity.kt
@@ -1,21 +1,34 @@
 package com.x.projectxx
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
         val TAG = "MainActivity"
     }
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Access a Cloud Firestore instance from your Activity
-        val db = Firebase.firestore
+
+    }
+
+    private fun testCreateUser () {
 
         val user = hashMapOf(
             "first" to "Ada",
@@ -33,5 +46,35 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "Error adding document", e)
             }
 
+    }
+
+    private fun faceBookLogin () {
+        FacebookSdk.sdkInitialize(applicationContext);
+        AppEventsLogger.activateApp(this);
+
+
+        val EMAIL = "email"
+
+//        val loginButton = findViewById<LoginButton>(R.id.login_button)
+//        loginButton.setReadPermissions(listOf(EMAIL))
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+        // If you are using in a fragment, call loginButton.setFragment(this);
+
+        // Callback registration
+//        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
+//            override fun onSuccess(loginResult: LoginResult?) {
+//                // App code
+//            }
+//
+//            override fun onCancel() {
+//                // App code
+//            }
+//
+//            override fun onError(exception: FacebookException) {
+//                // App code
+//            }
+//        })
     }
 }
