f747d0be44b3924bd9c202c404e2c15e5327a563
Billy <billhlaing@gmail.com>
2020-07-31 11:01:13 +1000

    firebase login with facebook working solution

--- app/src/main/java/com/x/projectxx/MainActivity.kt
+++ app/src/main/java/com/x/projectxx/MainActivity.kt
@@ -1,34 +1,43 @@
 package com.x.projectxx
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.facebook.FacebookCallback
-import com.facebook.FacebookException
-import com.facebook.FacebookSdk
+import com.facebook.*
 import com.facebook.appevents.AppEventsLogger
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.x.projectxx.databinding.ActivityMainBinding
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
     companion object {
-        val TAG = "MainActivity"
+        private const val TAG = "MainActivity"
     }
+
     val db = Firebase.firestore
+    val callbackManager = CallbackManager.Factory.create()
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         // Access a Cloud Firestore instance from your Activity
-
+        firebaseLoginWithFacebook()
     }
 
-    private fun testCreateUser () {
+    private fun testCreateUser() {
 
         val user = hashMapOf(
             "first" to "Ada",
@@ -48,33 +57,58 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun faceBookLogin () {
-        FacebookSdk.sdkInitialize(applicationContext);
-        AppEventsLogger.activateApp(this);
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
 
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
 
-        val EMAIL = "email"
+                    val user = auth.currentUser
+                    binding.displayName.text = user?.displayName
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
 
-//        val loginButton = findViewById<LoginButton>(R.id.login_button)
-//        loginButton.setReadPermissions(listOf(EMAIL))
-        // If you are using in a fragment, call loginButton.setFragment(this);
-
-        // Callback registration
-        // If you are using in a fragment, call loginButton.setFragment(this);
-
-        // Callback registration
-//        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult?> {
-//            override fun onSuccess(loginResult: LoginResult?) {
-//                // App code
-//            }
-//
-//            override fun onCancel() {
-//                // App code
-//            }
-//
-//            override fun onError(exception: FacebookException) {
-//                // App code
-//            }
-//        })
+                }
+            }
     }
+
+    fun firebaseLoginWithFacebook() {
+        // Initialize Facebook Login button
+
+        binding.loginButton.setReadPermissions("email", "public_profile")
+        binding.loginButton.registerCallback(
+            callbackManager,
+            object : FacebookCallback<LoginResult> {
+                override fun onSuccess(loginResult: LoginResult) {
+                    Log.d(TAG, "facebook:onSuccess:$loginResult")
+                    handleFacebookAccessToken(loginResult.accessToken)
+                }
+
+                override fun onCancel() {
+                    Log.d(TAG, "facebook:onCancel")
+                    // ...
+                }
+
+                override fun onError(error: FacebookException) {
+                    Log.d(TAG, "facebook:onError", error)
+                    // ...
+                }
+            })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+
 }
