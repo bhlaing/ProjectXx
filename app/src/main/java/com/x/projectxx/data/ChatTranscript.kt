2c3219b10da50fdc252b36ff2022f2b9f7863fa7
kyaw si thu <kyawsithu.1992@gmail.com>
2020-08-01 17:27:27 +1000

    Re-formatted dimens and init commit on chat activity and chatViewModel

---
+++ app/src/main/java/com/x/projectxx/data/ChatTranscript.kt
@@ -0,0 +1,3 @@
+package com.x.projectxx.data
+
+data class ChatTranscript(val transcripts:List<String>,val img:String)
---
+++ app/src/main/java/com/x/projectxx/ui/chat/ChatViewModel.kt
@@ -0,0 +1,7 @@
+package com.x.projectxx.ui.chat
+
+import androidx.lifecycle.ViewModel
+
+class ChatViewModel: ViewModel(){
+
+}
