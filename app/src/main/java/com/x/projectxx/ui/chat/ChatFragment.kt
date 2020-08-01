e4a8860be539d07f2aefe0f76d9477c2ab098549
kyaw si thu <kyawsithu.1992@gmail.com>
2020-08-01 10:49:04 +1000

    Implemented Layout for Chat Fragment

---
+++ app/src/main/java/com/x/projectxx/ui/chat/ChatFragment.kt
@@ -0,0 +1,19 @@
+package com.x.projectxx.ui.chat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.x.projectxx.databinding.ChatFragmentBinding
+
+class ChatFragment() : Fragment(){
+
+    private lateinit var binding: ChatFragmentBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = ChatFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+
+}
