cbdc4deea61e9f03f605678469dd8ba19d465b82
Billy <billhlaing@gmail.com>
2020-08-04 22:36:52 +1000

    Emit live data + Mock Data

--- app/src/main/java/com/x/projectxx/data/ChatTranscript.kt
+++ app/src/main/java/com/x/projectxx/data/ChatTranscript.kt
@@ -5,7 +5,9 @@ import java.util.*
 data class ChatTranscript(val messages: List<Message>) {
 
     // Set up for different type of messages to come
-    sealed class Message(val text: String, val timeStamp: Date, val fromUser: Sender, val image: String)
+    sealed class Message(val text: String, val timeStamp: Date, val fromUser: Sender, val image: String) {
+        class Text(text: String, timeStamp: Date, fromUser: Sender, image: String): Message(text, timeStamp, fromUser, image)
+    }
 
     sealed class Sender {
         object Myself: Sender()
