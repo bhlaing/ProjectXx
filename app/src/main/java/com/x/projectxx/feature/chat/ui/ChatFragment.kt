a3793b760f766afb67d93804322c76f6fbfaeab1
Joe Thu <kyawsithu.1992@gmail.com>
2020-08-06 21:47:46 +1000

    Added ChatMessagesAdapter and able to display chat messages from mock Messages

--- app/src/main/java/com/x/projectxx/feature/chat/ui/ChatFragment.kt
+++ app/src/main/java/com/x/projectxx/feature/chat/ui/ChatFragment.kt
@@ -5,14 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.x.projectxx.databinding.ChatFragmentBinding
+import com.x.projectxx.feature.chat.adapter.ChatMessagesAdapter
+import com.x.projectxx.global.extensions.observe
+import dagger.hilt.android.AndroidEntryPoint
 
-class ChatFragment() : Fragment(){
+@AndroidEntryPoint
+class ChatFragment : Fragment(){
 
     private lateinit var binding: ChatFragmentBinding
+    private val viewModel: ChatViewModel by viewModels()
+    private lateinit var chatMessageAdapter: ChatMessagesAdapter
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = ChatFragmentBinding.inflate(inflater)
+        binding.sendButton.setOnClickListener {
+            viewModel.messages
+        }
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setUpObservers()
+    }
+
+    private fun setupRecyclerView(){
+        binding.messageList.apply {
+            layoutManager = LinearLayoutManager(activity)
+            chatMessageAdapter = ChatMessagesAdapter()
+            adapter = chatMessageAdapter
+        }
+    }
+
+    private fun setUpObservers(){
+        viewLifecycleOwner.observe(viewModel.messages){ chatTranscript ->
+            chatTranscript?.messages?.let {
+                chatMessageAdapter.submitMessages(chatTranscript.messages)
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/x/projectxx/feature/chat/ui/ChatViewModel.kt
+++ app/src/main/java/com/x/projectxx/feature/chat/ui/ChatViewModel.kt
@@ -1,7 +1,9 @@
 package com.x.projectxx.feature.chat.ui
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.x.projectxx.feature.chat.usecase.GetChatTranscript
 
-class ChatViewModel: ViewModel(){
-
+class ChatViewModel @ViewModelInject constructor(usecase:GetChatTranscript): ViewModel(){
+    val messages = usecase.invoke(123)
 }
