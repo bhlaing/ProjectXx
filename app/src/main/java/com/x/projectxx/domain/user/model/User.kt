a44b8b17182dc4cc15175ba49db8d203e2d3341e
Billy <billhlaing@gmail.com>
2020-08-19 11:17:05 +1000

    - Formatting

--- app/src/main/java/com/x/projectxx/domain/user/model/User.kt
+++ app/src/main/java/com/x/projectxx/domain/user/model/User.kt
@@ -5,7 +5,6 @@ val displayName: String?,
 val image: String?,
 val email: String?,
 val status: String = "Life is good!",
-val contacts: List<Contact> = emptyList()
-) {
+val contacts: List<Contact> = emptyList()) {
     data class Contact(val userId: String)
 }
