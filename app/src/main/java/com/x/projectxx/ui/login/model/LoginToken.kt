d3ebd012abee202e564d631f154c0b279edfbab1
kyaw si thu <kyawsithu.1992@gmail.com>
2020-09-03 21:37:07 +1000

    Implemented firebase-auth and gms play services

--- app/src/main/java/com/x/projectxx/ui/login/model/LoginToken.kt
+++ app/src/main/java/com/x/projectxx/ui/login/model/LoginToken.kt
@@ -3,6 +3,6 @@ package com.x.projectxx.ui.login.model
 import com.facebook.login.LoginResult
 
 sealed class LoginToken{
-    class FacebookToken(var loginResult : LoginResult) : LoginToken()
-    class GoogleToken(var idToken : String) : LoginToken()
+    class FacebookToken(val loginResult : LoginResult) : LoginToken()
+    class GoogleToken(val idToken : String) : LoginToken()
 }
